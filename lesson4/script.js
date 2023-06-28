26d3943a6ed74fa3316a350387644b963de9077b
Damir Abdurakhimov <109365159+Damir94@users.noreply.github.com>
2023-06-28 15:03:22 -0400

    lesson4

---
+++ lesson4/script.js
@@ -0,0 +1,123 @@
+//Math methods
+/*console.log(Math.pow(3, 2));
+console.log(Math.sqrt(64));
+console.log(Math.cbrt(64));
+console.log(Math.max(10, 4, 37));
+console.log(Math.min(10, 4, 37));
+console.log(Math.abs(-5));
+console.log(Math.round(5,4));
+console.log(Math.random());
+console.log(Math.floor(Math.random() * 5));
+console.log(Math.ceil(4.001));
+
+//Function Declaration
+
+function example() {
+    console.log('Hey');
+}
+
+example();
+
+//Function Expression(Anonymous)
+
+let example = function() {
+    console.log('Hey');
+}
+
+example();
+
+//Arrow function
+
+let example = () => {
+    console.log('Hey');
+}
+
+example();
+//Function Declaration with return
+
+function example() {
+    return 'Hey';
+}
+
+console.log(example());
+//Function expression(Anonymous)
+
+let example = function() {
+    return 'Hey';
+}
+
+console.log(example());
+//Arrow function with return
+
+let example = () => {
+    return 'Hey'
+}
+
+console.log(example());
+
+//Auto return with arrow function
+
+let example = () => 'Hey'
+console.log(example());
+//Function with Parameter and Argument
+
+function person(name, age){
+    console.log('Hello ' + name + ' ' + age);
+}
+
+person('Mike', 27);
+function person(name = 'Default name') {
+    console.log(name);
+}
+
+person();
+function Mathpow(num, multiple) {
+    return num ** multiple;
+}
+
+console.log(pow(3, 2));
+
+function MathSqrt(num) {
+    return num ** 0.5;
+}
+
+console.log(MathSqrt(64));
+
+function abs(num) {
+    if (num < 0) {
+        return -num;
+    } else {
+        return num;
+    }
+}
+
+console.log(abs(10));
+
+
+function round(num) {
+    let integerPart = Number.parseInt(num);
+    let decimalPart = num - 0.5;
+    if (integerPart <= decimalPart) {
+        return Math.ceil(num);
+    } else {
+        return Math.floor(num);
+    }
+}
+
+console.log(round(5.3));
+function mathFloor(num) {
+    return Number.parseInt(num);
+}
+
+console.log(mathFloor(6.6));
+
+function mathCeil(num) {
+    if (Number.isInteger(num)) {
+        return num;
+    } else {
+        return Math.floor(num) + 1;
+    }
+    
+}
+console.log(mathCeil(4.011));
+*/
