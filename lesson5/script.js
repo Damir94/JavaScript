f4c5395219d2806942146c63631e243656dd098b
Damir Abdurakhimov <109365159+Damir94@users.noreply.github.com>
2023-06-29 14:23:22 -0400

    Add files via upload

---
+++ lesson5/script.js
@@ -0,0 +1,66 @@
+// String
+//syntax = '', "", ``
+/*let str = 'Webbrain';
+console.log(str[0]); //prints: W 
+
+let str = 'Webbrain';
+console.log(str.length); //prints: 8
+let str = 'Webbrain';
+console.log(str[str.length-1]); //prints: n
+let name = 'John';
+let status = 'Student';
+
+let str = `My name is ${name}, and I am a ${status}.`;
+console.log(str);
+
+let name = 'Webbrain \n Academy';
+console.log(name);
+
+let str = 'Webbrain';
+
+
+console.log(str.toLowerCase()); //prints: webbrain
+console.log(str.toUpperCase()); //prints: WEBBRAIN
+console.log(str.startsWith('W')); // prints: true
+console.log(str.endsWith('n')); //prints: true
+console.log(str.padStart(10, 'd')); //prints: ddWebbrain
+console.log(str.padEnd(10, '.')); // prints: Webbrain..
+console.log(str.replace('rain', 'sun')); //prints: Webbsun
+console.log(str.repeat(10)); 
+console.log(str.includes('brain')); //prints: true
+console.log(str.indexOf('b')); // prints: 2
+console.log(str.lastIndexOf('b')); //prints: 3
+console.log(str.charAt(7)); // prints: n
+console.log(str.substring(2,7)); // prints: bbrai
+console.log(str.slice(2, 7)); //prints: bbrai
+console.log(str.substr(2, 5)); // prints: bbrai
+
+
+
+let word = 'Webbrain '; 
+console.log(word.repeat(10));
+
+let word = 'Webbrain ';
+console.log(word.replace('Web', 'IT').repeat(10));
+
+
+let str = '#fozil#web#brain#';
+str = str.replace(/^#|#$|\s+/g, '');
+str = str.replace(/#/g, ')(');
+str = '(' + str + ')';
+
+console.log(str);
+
+
+let str = '     FoziL';
+
+let lastLetter = str.trim().slice(-1);
+
+if (lastLetter === 'L' || lastLetter === 'l') {
+    console.log(true);
+} else {
+    console.log(false);
+}
+*/
+
+
