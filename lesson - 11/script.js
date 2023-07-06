d1f158f79f9d21a1d2c0f560e133349fafdd0b5e
Damir Abdurakhimov <109365159+Damir94@users.noreply.github.com>
2023-07-06 13:06:51 -0400

    lesson11

---
+++ lesson - 11/script.js
@@ -0,0 +1,109 @@
+
+//Call
+
+/*let user = {
+    name: 'John',
+    sayHi: function(age, status) {
+        console.log(this.surname, age, status);
+    }
+};
+
+let user2 = {
+    surname: 'Doe'
+};
+user.sayHi.call(user2, 25, 'Mentor'); 
+
+
+
+//Apply 
+
+
+let user = {
+    name: 'John',
+    sayHi: function(age, status) {
+        console.log(this.surname, age, status);
+    }
+};
+
+let user2 = {
+    surname: 'Doe'
+};
+
+user.sayHi.apply(user2, [25, 'Mentor']);
+
+
+//Bind
+
+let user = {
+    name: 'John',
+    sayHi: function(status, age) {
+        console.log(this.surname, status, age);
+    }
+};
+
+let user2 = {
+    surname: 'Doe'
+};
+
+user.sayHi.bind(user2, 'Mentor', 25)();
+
+
+//Scope
+
+var num = 5;
+console.log(num); // prints: 5
+
+function check() {
+    num = 10
+};
+
+console.log(num); // prints: 5
+check();
+console.log(num); // prints: 10
+
+
+
+var num = 5;
+console.log(num); // prints: 5
+
+function check() {
+    var num = 10; // local variable
+    console.log(num);
+};
+
+console.log(num); // prints: 
+check(); // prints: 10
+console.log(num); // prints: 5
+
+
+
+const num = 5;
+
+function check() {
+    const num = 10; // local variable
+    console.log(num);
+};
+
+console.log(num); // prints: 5
+check(); // print: 10
+console.log(num); //prints: 5
+
+
+
+function check() {
+    if (true) {
+        var a = 'JavaScript';
+        let b = 'React';
+        const c = 'Angular';
+    }
+    console.log(a); // prints: JavaScript
+    console.log(b); // prints: b is not defined
+    console.log(c); // prints: c is not defined
+    
+};
+
+check();
+*/
+
+
+
