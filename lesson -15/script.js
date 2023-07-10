52aab60f9ac1e544e7cc68135cc39170dd165293
Damir Abdurakhimov <109365159+Damir94@users.noreply.github.com>
2023-07-10 11:30:36 -0400

    lesson15

---
+++ lesson -15/script.js
@@ -0,0 +1,42 @@
+
+//Error Handling
+
+/*const add = (a, b) => {
+    return a + b
+}
+
+try {
+    console.log(add(10, 20)); // prints: 30
+} catch(error) {
+    console.log(error);
+}
+
+
+//setTimeout 
+
+
+setTimeout(() => {
+    console.log('Hello group1');
+}, 3000);
+setTimeout(() => {
+    console.log('Hello group2');
+}, 2000);
+
+// prints: Hello group2
+//prints:  Hello group1
+
+
+//setInterval
+
+setInterval(() => {
+    let date = new Date();
+    console.log(`Current time: ${date.getHours()} : ${date.getMinutes()} : ${date.getSeconds()}`);
+}, 1000 );
+
+//prints: Current time: 11 : 18 : 54
+*/
+
+
+
+
+
