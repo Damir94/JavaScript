3e5d74418586537fc6a90c0f315c3fb4d10da527
Damir Abdurakhimov <109365159+Damir94@users.noreply.github.com>
2023-08-03 15:09:02 -0400

    Add files via upload

---
+++ JavaScript Loops/script.js
@@ -0,0 +1,173 @@
+/*
+const cars = ["BMW", "Volvo", "Saab", "Ford", "Fiat", "Audi"];
+
+let text = "";
+for (let i = 0; i < cars.length; i++) {
+    text += cars[i] + "<br>"
+}
+
+document.getElementById("demo").innerHTML = text;
+
+
+let text = "";
+
+for (let i = 0; i < 5; i++) {
+    text += "The number is " + i + "<br>";
+}
+
+document.getElementById("demo").innerHTML = text;
+
+
+
+const cars = ["BMW", "Volvo", "Saab", "Ford"];
+
+let i, len, text;
+
+for (i = 0, len = cars.length, text = ""; i < len; i++){
+    text += cars[i] + "<br>";
+}
+
+document.getElementById("demo").innerHTML = text;
+
+
+const cars = ["BMW", "Volvo", "Saab", "Ford"];
+let i = 2;
+let len = cars.length;
+let text = "";
+
+for (; i < len; i++) {
+    text += cars[i] + "<br>";
+}
+
+document.getElementById("demo").innerHTML = text;
+
+
+
+const cars = ["BMW", "Volvo", "Saab", "Ford"];
+
+let i = 0;
+let len = cars.length;
+let text = "";
+
+for (; i < len; ) {
+    text += cars[i] + "<br>";
+    i++;
+}
+
+document.getElementById("demo").innerHTML = text;
+
+
+
+var i = 5;
+for (var i = 0; i < 10; i++){
+    //some statements
+}
+document.getElementById("demo").innerHTML = i;
+
+
+
+let i = 5;
+
+for (let i = 0; i < 10; i++) {
+    //some code
+}
+document.getElementById("demo").innerHTML = i;
+
+
+
+const person = {fname: "John", lname: "Doe", age: 25};
+let txt = "";
+
+for (let key in person) {
+    txt += person[key] + " ";
+}
+document.getElementById("demo").innerHTML = txt;
+
+
+
+const numbers = [45, 4, 9, 16, 25];
+let txt = "";
+for (let key in numbers) {
+    txt += numbers[key] + "<br>";
+}
+document.getElementById("demo").innerHTML = txt;
+
+
+
+const numbers = [45, 4, 9, 16, 25];
+let txt = "";
+numbers.forEach(myFunction);
+document.getElementById("demo").innerHTML = txt;
+
+function myFunction(value, index, array) {
+    txt += value + "<br>";
+}
+
+
+
+const cars = ["BMW", "Volvo", "Mini"];
+
+let text = "";
+for (let x of cars) {
+    text += x + "<br>";
+}
+document.getElementById("demo").innerHTML = text;
+
+
+
+let language = "JavaScript";
+
+let txt = "";
+for (let x of language) {
+    txt += x + "<br>"
+}
+
+document.getElementById("demo").innerHTML = txt;
+
+
+
+let txt = "";
+let i = 0;
+while (i < 10) {
+    txt += "<br>The number is " + i;
+    i++;
+}
+document.getElementById("demo").innerHTML = txt;
+
+
+
+let txt = "";
+let i = 0;
+
+do {
+    txt += "<br>The number is " + i;
+    i++;
+}
+while (i < 10);
+document.getElementById("demo").innerHTML = txt;
+
+
+
+const cars = ["BMW", "Volvo", "Saab", "Ford"];
+
+let i = 0;
+let text = "";
+
+for (; cars[i];) {
+    text += cars[i] + "<br>";
+    i++;
+}
+document.getElementById("demo").innerHTML = text;
+
+
+
+const cars = ["BMW", "Volvo", "Saab", "Ford"];
+
+let i = 0;
+let text = "";
+while (cars[i]) {
+    text += cars[i] + "<br>";
+    i++;
+}
+document.getElementById("demo").innerHTML = text;
+*/
