0e4e422b2b2113234aa529430306c8bed526bb26
Damir Abdurakhimov <109365159+Damir94@users.noreply.github.com>
2023-08-16 12:12:12 -0400

    Add files via upload

---
+++ Project - Dog Years/app.js
@@ -0,0 +1,17 @@
+//create variable and set value equal to number
+const myAge = 29;
+//create variable let and set value equal to number
+let earlyYears = 2;
+earlyYears *= 10.5;
+// create new variable and set value equal to myAge - 2;
+let laterYears = myAge - 2;
+//multiplied it by 4
+laterYears *= 4;
+//added earlyYears and laterYears together to one new variable
+let myAgeInDogYears = earlyYears + laterYears;
+//create new variable myName and make it all lowercase letters
+let myName = 'Damir'.toLowerCase();
+//displayed the statement 
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
+
+
